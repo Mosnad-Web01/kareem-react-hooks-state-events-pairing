2903a805b0161a1db2e1317b629b4b790d78c31e
kareemAlqubati <salamkareem792@gmail.com>
2024-09-11 11:56:27 +0300

    Finishing

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import video from "../data/video.js";
+import Comments from "./Comments.js";
+import Video from "./Video.js";
 
 function App() {
   console.log("Here's your data:", video);
@@ -12,7 +14,12 @@ function App() {
         frameBorder="0"
         allowFullScreen
         title="Thinking in React"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       />
+      <h2>{video.title}</h2>
+      <p>Uploaded {video.createdAt} by {video.user} with {video.upvotes} upvotes and {video.downvotes} downvotes</p>
+      <br></br>
+      <Comments comments={video.comments} />
     </div>
   );
 }
